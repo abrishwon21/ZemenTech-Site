5866df836984cab743079819721cee4d2cdcaa71
Temam Hashim <52741393+Temam-Hashim@users.noreply.github.com>
2021-08-05 13:42:10 +0530

    Update blog.php

--- Home/partials/blog.php
+++ Home/partials/blog.php
@@ -1,3 +1,9 @@
+<?php 
+    require_once "includes/db.inc.php";
+    $sql = "SELECT * FROM `posts` where `status`='approved' Limit 10";
+    $result = mysqli_query($conn,$sql);
+?>
+
 <section id="blog" class="section">
     <div class="container">
         <div class="row">
@@ -7,52 +13,25 @@
             </div>
 
             <!-- Start Blog item #1-->
+         
             <div class="col-md-4">
                 <div class="blog-post">
+                <?php while($row = mysqli_fetch_assoc($result)){?>
                     <div class="post-media">
-                        <img src="images/blog/blog1.jpg" alt="">
+                        <img src="images/blog/<?php echo $row['p_photo'];?>" alt="">
                     </div>
                     <div class="post-desc">
-                        <h4>consectetur adipisicing Inventore</h4>
-                        <h5>12 May, 2015 / 5 Comments</h5>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, dolorum, fugiat,
-                            eligendi magni quibusdam iure cupiditate ex voluptas unde</p>
-                        <a href="blog.html" class="btn btn-gray-border">Read More</a>
+                        <h4><?php echo $row['p_title'];?></h4>
+                        <h5><?php echo $row['created'];?> / comment(<?php echo mysqli_num_rows($result);?>)</h5>
+                        <p><?php echo $row['p_description'];?></p>
+                        <a href="blog.php?post_id=<?php echo $row['p_id'];?>" class="btn btn-gray-border">Read More</a>
                     </div>
+                    <?php } ?>
                 </div>
             </div>
+         
 
-            <!-- Start Blog item #2-->
-            <div class="col-md-4">
-                <div class="blog-post">
-                    <div class="post-media">
-                        <img src="images/blog/blog2.jpg" alt="">
-                    </div>
-                    <div class="post-desc">
-                        <h4>consectetur adipisicing Inventore</h4>
-                        <h5>12 May, 2015 / 3 Comments</h5>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, dolorum, fugiat,
-                            eligendi magni quibusdam iure cupiditate ex voluptas unde</p>
-                        <a href="blog.html" class="btn btn-gray-border">Read More</a>
-                    </div>
-                </div>
-            </div>
 
-            <!-- Start Blog item #3-->
-            <div class="col-md-4">
-                <div class="blog-post">
-                    <div class="post-media">
-                        <img src="images/blog/blog3.jpg" alt="">
-                    </div>
-                    <div class="post-desc">
-                        <h4>consectetur adipisicing Inventore</h4>
-                        <h5>12 May, 2015 / 11 Comments</h5>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, dolorum, fugiat,
-                            eligendi magni quibusdam iure cupiditate ex voluptas unde</p>
-                        <a href="blog.html" class="btn btn-gray-border">Read More</a>
-                    </div>
-                </div>
-            </div>
 
         </div>
         <!--/.row-->
